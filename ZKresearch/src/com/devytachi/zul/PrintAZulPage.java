13c473fbbd5433fe956a4bba6337e78f2ce392c4
Ytalo Elias Borja Mori <ytaloborjam@gmail.com>
2013-08-28 15:45:24 -0500

    Test para imprimir zk page mediante un boton Print sin utilizar jasper reports. Este ejemplo fue tomado de http://online.odoerfer.com/OdKatalog/tree.zul#pi_30238, gracias por su apoyo

---
+++ ZKresearch/src/com/devytachi/zul/PrintAZulPage.java
@@ -0,0 +1,15 @@
+package com.devytachi.zul;
+
+import org.zkoss.zul.Button;
+
+import com.devytachi.zk.BaseWindow;
+
+
+@SuppressWarnings("serial")
+public class PrintAZulPage extends BaseWindow{
+
+	Button btnPrintThis;
+	public void onCreate(){
+	}
+	
+}
